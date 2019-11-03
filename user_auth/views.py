86c64127807db48d50d4abf0cfbeac9f0d19ffa9
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-11-03 08:28:26 +0530

    create views

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+
+@api_view(['GET'])
+def current_user(request):
+    """
+    Determine the current user by their token, and return their data
+    """
+    
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class CreateUserView(APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        user = request.data.get('user')
+        if not user:
+            return Response({'response' : 'error', 'message' : 'No data found'})
+        serializer = UserSerializerWithToken(data = user)
+        if serializer.is_valid():
+            saved_user = serializer.save()
+        else:
+            return Response({"response" : "error", "message" : serializer.errors})
+        return Response({"response" : "success", "message" : "user created succesfully"})
